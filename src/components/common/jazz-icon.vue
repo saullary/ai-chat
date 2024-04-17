7264c8cf63a930e48d00a3f59fe6c5d0e21dace0
hyongnim <409337645@qq.com>
2024-04-17 17:13:56 +0800

    ui

---
+++ src/components/common/jazz-icon.vue
@@ -0,0 +1,49 @@
+<template>
+  <div ref="content" class="al-c"></div>
+</template>
+
+<script>
+import jazzicon from "jazzicon";
+
+export default {
+  props: {
+    hash: String,
+    size: {
+      default: 22,
+      type: Number,
+    },
+  },
+  data() {
+    return {
+      iconDom: null,
+    };
+  },
+  watch: {
+    hash() {
+      this.getIcon();
+    },
+  },
+  mounted() {
+    this.getIcon();
+  },
+  computed: {
+    addrSeed() {
+      const addr = (this.hash || "").slice(2, 10);
+      const seed = parseInt(addr, 16);
+      return seed;
+    },
+  },
+  methods: {
+    getIcon() {
+      if (!this.hash) return;
+      const identicon = jazzicon(this.size, this.addrSeed);
+      this.iconDom = identicon.cloneNode(true);
+      const $con = this.$refs.content;
+      [].slice.call($con.children).forEach((el) => {
+        $con.removeChild(el);
+      });
+      $con.appendChild(this.iconDom);
+    },
+  },
+};
+</script>
--- src/layouts/drawer-view.vue
+++ src/layouts/drawer-view.vue
@@ -2,12 +2,23 @@
   <div>
     <div class="pa-3 pos-s top-0 bdb-1" style="background: #fcf9ff">
       <div class="al-c">
-        <img src="/img/model.svg" width="26" />
+        <img src="/img/model.svg" width="30" />
         <span class="fz-18 ml-2">Model</span>
+        <q-btn class="ml-auto" color="primary" dense unelevated>
+          <q-icon name="add_circle_outline" size="20px"></q-icon>
+        </q-btn>
+        <q-btn class="ml-3 bg-white bd-1" dense flat>
+          <img src="/img/trash.svg" width="22" class="px-2p" />
+        </q-btn>
       </div>
     </div>
-    <div>
-      <p v-for="i in 30" :key="i">{{ i }}</p>
+    <div class="pa-3">
+      <div class="bd-1 bg-white px-3 py-2 bdrs-6">
+        <div class="al-c">
+          <jazz-icon hash="test" :size="28"></jazz-icon>
+          <span class="ml-2">Chat GPT-3.5</span>
+        </div>
+      </div>
     </div>
   </div>
 </template>
