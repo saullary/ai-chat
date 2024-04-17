0fccc1832645274721e7f2835fd29299c662cd40
hyongnim <409337645@qq.com>
2024-04-17 20:37:05 +0800

    d

--- src/pages/chat/page-chat.vue
+++ src/pages/chat/page-chat.vue
@@ -1,5 +1,30 @@
 <template>
   <div class="al-c pa-3 bdb-1">
     <span class="fz-18">Chat</span>
+    <q-select
+      style="width: 150px; height: 30px"
+      class="ml-auto"
+      v-model="apiKey"
+      outlined
+      dense
+      :options="keyOpts"
+      map-options
+    ></q-select>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      apiKey: "",
+      keyOpts: [
+        {
+          label: "test",
+          value: "11",
+        },
+      ],
+    };
+  },
+};
+</script>
