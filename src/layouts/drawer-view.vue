15d78279a6501fe38d4113bbc6680a93a1ec074e
hyongnim <409337645@qq.com>
2024-04-22 13:02:36 +0800

    ui

--- src/layouts/drawer-view.vue
+++ src/layouts/drawer-view.vue
@@ -1,13 +1,36 @@
+<style lang="scss">
+.model-item {
+  &.check {
+    background: #fff;
+    &:hover {
+      background: #f3e8ff;
+      border-color: #775da655;
+      border-color: rgb(119 93 166 / 30%);
+    }
+  }
+  &.uncheck {
+    background: #f8fafc;
+    border: 1px dashed #cbd5e1;
+    color: #94a3b8;
+    &:hover {
+      background: #f3e8ff;
+      border-color: #775da655;
+      border-color: rgb(119 93 166 / 35%);
+    }
+  }
+}
+</style>
+
 <template>
-  <div>
-    <div class="pa-3 pos-s top-0 bdb-1" style="background: #fcf9ff">
+  <div class="h-flex h100p">
+    <div class="pa-3 bdb-1" style="background: #fcf9ff">
       <div class="al-c">
         <img src="/img/model.svg" width="30" />
         <span class="fz-18 ml-2">Model</span>
 
         <q-btn class="ml-auto" color="primary" dense unelevated>
           <q-icon name="add_circle_outline" size="20px"></q-icon>
-          <q-menu>
+          <q-menu max-height="360px">
             <q-list dense separator style="min-width: 160px">
               <template v-for="group in modelGroups" :key="group.name">
                 <div class="gray fz-12 px-4 py-2">
@@ -36,18 +59,31 @@
         </q-btn> -->
       </div>
     </div>
-    <div class="pa-3">
-      <div
-        v-for="it in modelOptions"
-        :key="it.id"
-        class="bd-1 bg-white px-3 py-2 bdrs-6 mb-3"
-      >
-        <div class="al-c">
-          <jazz-icon :hash="it.id" :size="30"></jazz-icon>
-          <span class="ml-2">{{ it.name }}</span>
+    <q-scroll-area
+      class="flex-1"
+      :thumb-style="{
+        right: '2px',
+        width: '3px',
+        opacity: 0.35,
+      }"
+    >
+      <div class="pa-3">
+        <div
+          v-for="it in modelOptions"
+          :key="it.id"
+          class="bd-1 px-3 py-2 bdrs-6 mb-3 model-item pos-r"
+          :class="checked.includes(it.id) ? 'check' : 'uncheck'"
+        >
+          <div class="al-c">
+            <jazz-icon :hash="it.id" :size="30"></jazz-icon>
+            <span class="ml-2">{{ it.name }}</span>
+          </div>
+          <div class="y-center right-0 mr-3 pa- bg-white bdrs-6">
+            <q-toggle v-model="checked" :val="it.id" size="sm" />
+          </div>
         </div>
       </div>
-    </div>
+    </q-scroll-area>
   </div>
 </template>
 
@@ -98,6 +134,7 @@ export default {
     },
   },
   created() {
+    this.checked = JSON.parse(localStorage.checkedModels || "[]");
     this.getModels();
   },
   methods: {
